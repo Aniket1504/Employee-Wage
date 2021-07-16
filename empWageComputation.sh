cb8df9c6e8f25177ab40917bf34f6cbfcdaec5d8
Aniket <aniketshimgekar76@gmail.com>
2021-07-16 12:26:40 +0530

    modified : added features for wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,14 @@
 #! /bin/bash -x
 
-echo "Welcome to EmployeeWage Problem"
+IS_Present=1
+
+empCheck=$(( RANDOM % 2 ))
+EMP_RATE_PER_HR=20
+if [ $empCheck -eq $IS_PRESENT ]
+then
+    empHrs=8
+else
+    empHrs=0
+fi
+
+salary=$(( $empHrs * $EMP_RATE_PER_HR ))
