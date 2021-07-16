75d36b499d437324ff47eb6e870a76216db137cf
Aniket1504 <84281816+Aniket1504@users.noreply.github.com>
2021-07-16 12:50:22 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,16 +13,3 @@ then
 else
     echo "employee is absent"
 fi
-
-IS_Present=1
-
-empCheck=$(( RANDOM % 2 ))
-EMP_RATE_PER_HR=20
-if [ $empCheck -eq $IS_PRESENT ]
-then
-    empHrs=8
-else
-    empHrs=0
-fi
-
-salary=$(( $empHrs * $EMP_RATE_PER_HR )
