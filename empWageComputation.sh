454be57b7d5890d30cce3ff9cf07211aae26b8b7
Aniket <aniketshimgekar76@gmail.com>
2021-07-16 14:24:33 +0530

    modified : added feature for calculating per month salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,20 +4,23 @@
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
 WAGE_PER_HR=20
+Max_Work_Days=20
+for ((day=1;day<=$Max_Work_Days;day++))
+do
+       #variable
+       empCheck=$((RANDOM%3))
 
-#variable
-empCheck=$((RANDOM%3))
-
-# selection
-if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
-then
+       # selection
+       if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
+       then
 		empHrs=8
-elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
-then
+       elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
+       then
                 empHrs=4
-else
+       else
 		empHrs=0
-fi
-#calculation
-salary=$(( $empHrs * $WAGE_PER_HR ))
-echo $salary
+       fi
+      #calculation
+      salary=$(( $empHrs * $WAGE_PER_HR ))
+      echo $salary
+done
